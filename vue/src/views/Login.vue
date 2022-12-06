396524bbd1d2c893c5e9c995a059c96eff82d6cd
Mikey Nguyen <mikengoc@gmail.com>
2022-12-06 08:05:33 -0500

    attempted to add style to Login.vue

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,7 +1,9 @@
-<template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+<template> 
+ <body>
+  <form class="form-signin" @submit.prevent="login">
+    <!---------- consider breaking this intro into h2 tag to style or do it inline --------->
+      <h1>welcome to taste buds<br/>where the first bite could change your life</h1>
+      
       <div
         class="alert alert-danger"
         role="alert"
@@ -12,7 +14,7 @@
         role="alert"
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
+      <label for="username" class="sr-only">Email</label>
       <input
         type="text"
         id="username"
@@ -34,7 +36,8 @@
       <router-link :to="{ name: 'register' }">Need an account?</router-link>
       <button type="submit">Sign in</button>
     </form>
-  </div>
+  </body>
+  
 </template>
 
 <script>
@@ -64,6 +67,7 @@ export default {
           }
         })
         .catch(error => {
+          alert(error);
           const response = error.response;
 
           if (response.status === 401) {
@@ -74,3 +78,70 @@ export default {
   }
 };
 </script>
+
+
+ <style scoped>
+body{
+  background-color: rgb(255, 255, 235);
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  justify-content: space-between;
+  gap: 25px;
+  
+  /* original color: rgba(255, 255, 221, 0.867); */
+}
+
+
+h1 {
+  text-align: center;
+}
+
+form.form-signin{
+  order: 2;
+  flex-basis: 33%;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+input.form-control {
+  width: 80%;
+  padding: 10px 15px;
+  border: 3px solid lightgray;
+  border-radius: 10px;
+  font-size: 16px;
+}
+input.form-control:focus {
+  border: 3px solid black;
+}
+button.submit {
+  border: none;
+  background-color: rgb(243, 214, 243);
+  font-size: 20px;
+  border-radius: 25px;
+  width: 250px;
+  padding: 12px 32px;
+}
+button.submit:hover {
+  background-color: plum;
+  color: white;
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+/* SCREEN DIFFERENCES */
+@media screen and (max-width: 400px) {
+  body {
+      display:flex;
+      flex-direction: column;
+    }
+  
+    
+}
+@media screen and (max-width: 800px) and (min-width: 400px) {
+  body {
+    display:flex;
+    flex-direction: column;
+  }
+  
+}
+</style>
