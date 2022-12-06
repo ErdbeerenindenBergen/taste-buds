1711888fd8750238ceaca3d5d6a4aa86160938b7
Kassi Burnett <burnett.126@osu.edu>
2022-12-06 18:53:51 -0500

    updating database SQL

--- java/database/tastebuds.sql
+++ java/database/tastebuds.sql
@@ -17,6 +17,9 @@ CREATE TABLE tb_user(
 	CONSTRAINT UQ_username UNIQUE (username)
 );
 
+INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+
 CREATE SEQUENCE seq_event_id
 INCREMENT BY 1
 START WITH 1001
@@ -58,5 +61,32 @@ CREATE TABLE event_restaurant(
 	vote_count INT NOT NULL,
 	CONSTRAINT PK_event_restaurant PRIMARY KEY (yelp_restaurant_id,event_id)	
 );
+
+DROP USER IF EXISTS final_capstone_owner;
+DROP USER IF EXISTS final_capstone_appuser;
+
+CREATE USER final_capstone_owner
+WITH PASSWORD 'finalcapstone';
+
+GRANT ALL
+ON ALL TABLES IN SCHEMA public
+TO final_capstone_owner;
+
+GRANT ALL
+ON ALL SEQUENCES IN SCHEMA public
+TO final_capstone_owner;
+
+CREATE USER final_capstone_appuser
+WITH PASSWORD 'finalcapstone';
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON ALL TABLES IN SCHEMA public
+TO final_capstone_appuser;
+
+GRANT USAGE, SELECT
+ON ALL SEQUENCES IN SCHEMA public
+TO final_capstone_appuser;
+
+COMMIT TRANSACTION;
 
 COMMIT;
