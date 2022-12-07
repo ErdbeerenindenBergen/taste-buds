0665af30f25b0640371c427031ea962f73269680
KApolinario1120 <kevinsquared@gmail.com>
2022-12-06 20:04:15 -0500

    Removed email functionality from User classes. Removed event invite and restauraunt classes

--- java/src/main/java/com/techelevator/controller/EventController.java
+++ java/src/main/java/com/techelevator/controller/EventController.java
@@ -1,9 +1,6 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.EventDao;
-import com.techelevator.model.Event;
-import com.techelevator.model.EventInvitation;
-import com.techelevator.model.EventRestaurant;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
