026aa0f1cf3a57ea10e36e46d3a02709ec1d2825
Mikey Nguyen <mikengoc@gmail.com>
2022-12-08 08:10:06 -0500

    Moved compnent contents to view

--- vue/src/views/InviteBuds.vue
+++ vue/src/views/InviteBuds.vue
@@ -1,15 +1,69 @@
 <template>
-<div class="invite-buds">
-    <h1>Invite Placeholder</h1>
-  </div>
+    <div id="invite-buds">
+
+                    <!---------------------Form Left Panel ---------------------->
+
+        <form class="invite-form" @submit="invite">
+           
+            <div class="event-info">
+                <h2>Event Name:</h2>
+                <input type="text" class="event-name" v-model="eventName" placeholder="Capstone Celebration" />
+            </div>
+
+            <div class="location">
+                <h2>Enter a city or zipcode:</h2>
+                <input type="text" class="location" v-model="location" placeholder="City or Zipcode" />
+            </div>
+
+            <div class="date-time">
+                <h2>When must your buds respond by?</h2>
+                <input type="text" class="date" v-model="date" placeholder="mm/dd/yyyy"/>
+                <input type="text" class="time" v-model="time" placeholder="hh:mm" />
+            </div>
+
+            <button type="button" id="event-info-button" v-on:click="find()">SUBMIT</button>
+            
+                    <!-------------------View all Restaurants Button/Left Panel ----------------->
+                    //this list either shows up on the invite view page or takes them back to find a restaurant? 
+            <button type="button" id="restaurant-button" v-on:click="viewRestaurants()">View Restaurants List</button>
+
+                    <!-------------------Send Invite Button Button/Right Panel ------------------>
+            <div class="send-invite">
+                <h2>Want to invite a friend who is not yet a taste bud?</h2>
+                <button type="button" id="invite-button" v-on:click="sendInvite()">send an invite!</button>
+            </div>
+        
+        </form>
+    </div>
 </template>
+
+
 
 <script>
 
-export default {
-  name: "invite-buds"
-};
+export default{
+
+    //mikey note to self: think about imports and whether u need them
+    
+    data() {
+        return {
+            restaurantOptions: [],//this is for 'view restaurants list' button?]
+            inviteId: "",
+            location: "",
+            decisionDate: "", //might have to parse LocalDate as string
+            decisionTime: "",//might have to parse LocalTime as string
+            uniqueInvitationLink: "",
+
+        }
+    },
+
+
+        
+    
+}
+
 </script>
+
 
 <style scoped>
 h1{
