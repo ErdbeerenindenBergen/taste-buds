6b18a2278f5ba7e967ff293674fce5004b68893f
Mikey Nguyen <mikengoc@gmail.com>
2022-12-11 00:19:41 -0500

    the beggining of Events view

--- vue/src/services/EventService.js
+++ vue/src/services/EventService.js
@@ -8,5 +8,17 @@ export default {
 
     createEvent(event){
         return http.post(`/event/create`, event)
+    },
+
+    getEventById(eventId){
+        return http.get(`/${eventId}`)
+    },
+
+    getEventByName(eventName){
+        return http.get('/eventName', eventName)
+    },
+
+    getAllEvents(){
+        return http.get()//does this have an endpoint?
     }
 }
