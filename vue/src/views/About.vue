3884c2b91837f1f259c9761141720ada666a7236
Brittany Weaver <brittanyweaver13@gmail.com>
2022-12-12 10:52:48 -0500

    working on about us

--- vue/src/views/About.vue
+++ vue/src/views/About.vue
@@ -1,6 +1,41 @@
 <template>
-  <div class="about">
-    <h1>About Placeholder</h1>
+  <div class="about-us">
+    <div class="about-title">
+      <h1>Meet the taste buds Team</h1>
+    </div>
+
+    <div class="Kassi">
+      <h3>Kassi Burnett</h3>
+      <img src="@/assets/Kassi.png" alt="Kassi Burnett"/>
+      <p>
+      </p>
+    </div>
+
+     <div class="Brittany">
+      <h3>Brittany Weaver</h3>
+      <p>
+      </p>
+    </div>
+
+    <div class="Mikey">
+      <h3>Mikey Nguyen</h3>
+      <p>
+      </p>
+    </div>
+
+     <div class="Matthew">
+      <h3>Matthew Reynolds</h3>
+      <p>
+      </p>
+    </div>
+
+
+   <div class="Kevin">
+      <h3>Kevin Apolinario</h3>
+      <p>
+      </p>
+    </div>
+
   </div>
 </template>
 
