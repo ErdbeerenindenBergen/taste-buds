4b12408f3b52284f1f024cf9885d0ce58bc7e8ea
Mikey Nguyen <mikengoc@gmail.com>
2022-12-12 16:20:54 -0500

    more mikey

--- vue/src/views/Events.vue
+++ vue/src/views/Events.vue
@@ -19,11 +19,116 @@
       </button>
     </div>
 
-    <!---------------- Events Info ------------------>
+    <!---------------- View Event Restaurant ------------------>
     <h3>Events:</h3>
     <div id="event-name-info" v-for="event in events" :key="event.id">
       <h2> {{ event.eventName }}</h2>
     </div>
+
+    <!---------------- Event Restaurant Info ------------------>
+    <div id="root">
+    <link rel="preconnect" href="https://fonts.googleapis.com" /><link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossorigin
+    /><link
+      href="https://fonts.googleapis.com/css2?family=Playfair+Display"
+      rel="stylesheet"
+    />
+
+    <div
+      id="restaurant-display"
+      v-for="business in business"
+      :key="business.id"
+    >
+      <div id="restaurant-left">
+        <h2 id="name">{{ business.name }}</h2>
+
+        <img
+          v-if="business.image_url != ''"
+          :src="business.image_url"
+          class="thumbnail"
+        />
+
+        <div id="icons">
+        
+
+          <div id="yelp-icon-link">
+            <a id="url" :href="`${business.url}`" target="_blank">
+              <img
+                id="yelp-icon"
+                src="@/assets/yelp.png"
+                alt="yelp-icon"
+                class="zoom"
+              />
+            </a>
+          </div>
+
+          <div id="google-icon-link">
+            <a
+              id="url"
+              :href="
+                'http://maps.google.com/?q=' +
+                `${business.name}` +
+                `${business.location.display_address[0]}`
+              "
+              target="_blank"
+            >
+              <img
+                id="google-icon"
+                src="@/assets/google-maps.png"
+                alt="google-maps-icon"
+                class="zoom"
+              />
+            </a>
+          </div>
+
+        </div>
+      </div>
+
+      <div id="restaurant-right">
+        <h2 id="stars">Average rating: {{ business.rating }}</h2>
+        <h2 id="isClosed">
+          {{ business.is_closed === false ? "Open now" : "Closed" }}
+        </h2>
+
+        <div id="contact-information">
+          <p id="contacts">{{ business.display_phone }}</p>
+          <p id="address1">{{ business.location.display_address[0] }}</p>
+          <p id="address2">{{ business.location.display_address[1] }}</p>
+        </div>
+
+        <div id="pickup-or-delivery">
+          <p id="pickup">
+            {{
+              business.transactions.includes("pickup")
+                ? "Pick-up is available"
+                : ""
+            }}
+          </p>
+          <p id="delivery">
+            {{
+              business.transactions.includes("delivery")
+                ? "Delivery is available"
+                : ""
+            }}
+          </p>
+        </div>
+
+        <div id="phone-button">
+          <a href="tel:${business.phone}" target="_blank"
+            ><button class="call-button" type="button call">
+              <font-awesome-icon icon="phone" /> CALL TO ORDER
+            </button></a
+          >
+        </div>
+      </div>
+    </div>
+   </div>
+
+    <!---------------- View Invitees ------------------>
+
+
   </div>
 </template>
 
@@ -57,7 +162,6 @@ export default {
       
       return this.$store.state.approvedRestaurants;//trying to get the approvedRestaurants list from the store
       
-      // return this.$store.state.restaurants;
     },
     showViewInvitees() {
       const stepOneForm = document.getElementById('view-restaurants');
